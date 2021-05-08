502bbe8485d4950f1a8bc4040ca39330466dbbca
kazirafi71 <kazirafi66@gmail.com>
2021-05-09 02:58:11 +0600

    teacher dashboar

--- client/src/pages/Teacher/TeacherDashboard/TeacherDashboard.js
+++ client/src/pages/Teacher/TeacherDashboard/TeacherDashboard.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Col, Container } from 'react-bootstrap'
+import { Paper, Typography } from '@material-ui/core'
 
 const TeacherDashboard = () => {
     return (
         <div>
-            TeacherDashboard
+            <Container>
+            <Col md={10} className='mx-auto'>
+          <Container>
+            <Paper>
+              <Typography
+                className="text-center text-primary py-5"
+                variant="h4"
+              >
+                Welcome to Teacher Dashboard
+                
+              </Typography>
+            </Paper>
+          </Container>
+        </Col>
+
+            </Container>
         </div>
     );
 };
